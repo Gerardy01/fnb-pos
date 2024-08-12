fb65324909133e9daa76d77afecc86a50d13ddcf
Gerardy Lucas <ardy.paudianto@gmail.com>
2024-08-12 15:51:54 +0700

    add new folder and create organization and role models

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,6 +13,7 @@ const port = process.env.PORT || 8000;
 // Middleware
 app.use(express.json());
 
+
 app.use("/api", api);
 
 app.use("*", (req, res) => res.status(404).send("NO API ROUTES"));
---
+++ server/src/models/role.model.ts
@@ -0,0 +1,49 @@
+import { Sequelize, DataType, Model, DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+// models
+import Organization from "./organization.model";
+
+
+class Role extends Model {
+    public role_id! : number;
+    public role_name! : string;
+    public is_default! : boolean;
+    public organization_id! : string;
+}
+
+Role.init({
+    role_id: {
+        type: DataTypes.INTEGER,
+        unique: true,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    role_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    is_default: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+    organization_id: {
+        type: DataTypes.UUIDV4,
+        allowNull: true,
+        references: {
+            model: Organization,
+            key: 'organization_id',
+        },
+        onDelete: 'CASCADE'
+    },
+}, {
+    sequelize,
+    modelName: 'Role',
+    tableName: 'role',
+    timestamps: true,
+});
+
+export default Role;
+
