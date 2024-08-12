744e34176194d531822906ee166b7b34c1a2bf15
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2024-08-12 23:10:56 +0700

    create migrations file

---
+++ server/src/migrations/20240812154842-create-account.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('account', {
+      account_id: {
+        type: Sequelize.UUID,
+        unique: true,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING(20),
+        unique: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      organization_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'organization',
+          key: 'organization_id',
+        },
+        onDelete: 'CASCADE',
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      archived: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('account');
+  }
+};
---
+++ server/src/migrations/20240812155150-create-organization.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('organization', {
+      organization_id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      organization_logo: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      organization_no: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        unique: true,
+      },
+      archived: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      end_valid_datetime: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('organization');
+  },
+};
