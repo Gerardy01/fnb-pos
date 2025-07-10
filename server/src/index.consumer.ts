3bea4826500239698dfd90c12d28ba11d70f3a8c
Gerardy Lucas <ardy.paudianto@gmail.com>
2025-07-10 09:37:43 +0700

    split runtime between queue consumer and main app

---
+++ server/src/index.consumer.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express";
+
+// Queue
+import { connectRabbitMQ } from './config/rabbitmq';
+import { consumeEmailQueue } from "./queue/emailConsumer";
+
+
+const app : Express = express();
+const port : number = Number(process.env.CONSUMER_PORT) || 80;
+const host : string = process.env.CONSUMER_HOST || '0.0.0.0'; ;
+
+
+// Queue Consumer runtime
+(async () => {
+    await connectRabbitMQ();
+    await consumeEmailQueue();
+
+    app.listen(port, host, () => {
+        console.log(`[server]: Server is running at http://${host}:${port}`);
+    });
+})();
